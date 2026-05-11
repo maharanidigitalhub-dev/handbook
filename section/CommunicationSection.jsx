0529192e30b17186a0926599b200f00916c5588e
maharanidigitalhub-dev <maharanidigitalhub@gmail.com>
2026-05-11 12:43:56 +0800

    Update CommunicationSection.jsx

--- section/CommunicationSection.jsx
+++ section/CommunicationSection.jsx
@@ -1,5 +1,5 @@
-import { V } from '../tokens.js';
-import { Callout, SectionTitle, SectionHeading, TblWrap, Th, Td, TrBody, EditInput, EditSelect } from '../components.jsx';
+import { V } from '../src/tokens.js';
+import { Callout, SectionTitle, SectionHeading, TblWrap, Th, Td, TrBody, EditInput, EditSelect } from '../CSS/components.jsx';
 
 const CommunicationSection = () => (
   <div className="fade-up section-pad" style={{ padding: '32px 36px' }}>
